bdf90861c78a8e40f85a6e03810ca89c826cd171
elfrinjo <elfrinjo@e8b8927f-3092-99b5-3301-b6af723087a2>
2010-07-24 11:27:43 +0000

    english translation

--- core/g2fbook.sh
+++ core/g2fbook.sh
@@ -3,35 +3,34 @@
 ##
 #	g2fbox.sh
 #
-# Konvertierung von google contacts in ein xml Format das als Telefonbuchbackup
-# fuer eine AVM Fritz!Box verwendet werden kann.
+# Something to download google contacts and to reuse them in other formats.
 #
-# Systemvoraussetzungen
-# GNU Linux / Cygwin Umgebung mit folgenden Tools im Pfad:
+# Requirements
+# GNU Linux / Cygwin Umgebung with these tools in path:
 #  -bash
 #  -curl
 #  -tr
 #  -grep
-#  -xsltproc (gehoert zu Gnome libxslt)
+#  -xsltproc (contained in gnome libxslt)
 #
 #
 #########################################################################################
 #     
 #     Copyright (C) 2010 J. Elfring
 #     
-#     Dieses Programm ist freie Software. Sie koennen es unter den Bedingungen der
-#     GNU General Public License, wie von der Free Software Foundation veroeffentlicht,
-#     weitergeben und/oder modifizieren, entweder gemaess Version 3 der Lizenz oder
-#     (nach Ihrer Wahl) jeder spaeteren Version.
-#     
-#     Die Veroeffentlichung dieses Programms erfolgt in der Hoffnung, dass es Ihnen von
-#     Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die implizite
-#     Garantie der MARKTREIFE oder der VERWENDBARKEIT FUER EINEN BESTIMMTEN ZWECK.
-#     Details finden Sie in der GNU General Public License.
-#     
-#     Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem
-#     Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
-#     
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#  
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#  
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 #########################################################################################
 
 option=$1
@@ -39,30 +38,29 @@ googleEmail=$2
 googlePass=$3
 
 echo ""
-echo "g2fbook.sh - Copyright (C) 2010 J. Elfring"
-echo "Fuer dieses Programm besteht KEINERLEI GARANTIE."
-echo "Dies ist freie Software, die Sie unter bestimmten Bedingungen weitergeben duerfen."
-echo "Fuer weitere Details schauen Sie bitte in dieses File."
+echo "*g2fbook.sh - Copyright (C) 2010 J. Elfring"
+echo "*This program comes with ABSOLUTELY NO WARRANTY."
+echo "*This is free software, and you are welcome to redistribute it"
+echo "*for further details please look into this file."
 echo ""
 
-
-# Hilfe
+# Help!
 if [ $# != 3 -o  _$1 != _F -a _$1 != _G -a _$1 != _B ]
 then
-	echo " Aufruf:"
+	echo " Invocation:"
 	echo " $ sh $0 {F|G|B} username@gmail.com password "
-	echo "   F - Erstellung des Backupfiles fuer die Fritz!Box"
-	echo "   G - Erstellung des Backupfiles fuer gammu (Mobiltelefonbuch Backup)"
-	echo "   B - Erstellt beide Files"
-	echo "   Username und Passwort fuer Ihre Anmeldung bei google"
+	echo "   F - Create phone book backup for AVM Fritz!Box"
+	echo "   G - Create backup file for gammu/wammu (mobile phone book backup)"
+	echo "   B - Create both files"
+	echo "   username and password for your google account"
 	echo ""
 	exit 99
 fi
 
 
 echo ""
-echo "Rufe Daten bei Google ab..."
-# Bei Google anmelden
+echo "Downloading data from google..."
+# Google Auth
 googleAuth=$(curl https://www.google.com/accounts/ClientLogin \
 		--data Email=$googleEmail -d Passwd=$googlePass \
 		--data accountType=HOSTED_OR_GOOGLE \
@@ -74,13 +72,13 @@ googleAuth=$(curl https://www.google.com/accounts/ClientLogin \
 		)
 if [ $? != 0 ]
 then
-        echo "Fehler bei der Authentifizierung."
+        echo "Authentication failed."
 	echo $googleAuth
         exit 1
 fi
 authTag=`echo $googleAuth | tr [:space:] '\n' | grep -E '^Auth'`
 
-# Die XML-Kontaktliste abrufen
+# Get the contact feed
 curl http://www.google.com/m8/feeds/contacts/$googleEmail/full?max-results=99999 \
 	--header "Authorization: GoogleLogin $authTag" \
 	--silent \
@@ -89,20 +87,20 @@ curl http://www.google.com/m8/feeds/contacts/$googleEmail/full?max-results=99999
 	> googleContacts.$$.xml
 if [ $? != 0 ]
 then
-        echo "Fehler beim lesen der Kontakte"
+        echo "Download of contact feed failed"
         exit 2
 fi
 
 
 if [ $option == F -o $option == B ]
 then	
-	echo "Schreibe FritzBoxPhoneBook.xml..."
+	echo "Writing FritzBoxPhoneBook.xml..."
 	xsltproc googleContacts-2-fritzPhoneBook.xml googleContacts.$$.xml > FritzBoxPhoneBook.xml
 fi	
 
 if [ $option == G -o $option == B ]
 then	
-	echo "Schreibe GammuPhoneBookBackup.txt..."
+	echo "Writing GammuPhoneBookBackup.txt..."
 	xsltproc googleContacts-2-gammuPhoneBackup.xml googleContacts.$$.xml > GammuPhoneBookBackup.txt
 fi	
 
