9def1b6ee46338511bade0128213920502dc9700
rico <rico@engelrico.de>
2011-08-29 20:10:01 +0000

    GoogleService, PropertyService and functionality added

---
+++ src/de/g2fbook/services/I_PropertyService.java
@@ -0,0 +1,10 @@
+package de.g2fbook.services;
+
+public interface I_PropertyService {
+
+	public final static String PARAM_GOOGLE_USR="google_usr";
+	public final static String PARAM_GOOGLE_PWD="google_pwd";
+	public final static String PARAM_GOOGLE_URL_LOGIN="google_url_login";
+	public final static String PARAM_GOOGLE_URL_PBOOK="google_url_pbook";
+		
+}
