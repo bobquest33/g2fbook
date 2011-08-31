199e6abe3283aba0ca6a2b3b49b8e93dafa2b996
rico@engelrico.de <rico@engelrico.de@e8b8927f-3092-99b5-3301-b6af723087a2>
2011-08-31 20:13:51 +0000

    including database to persist phonebooks

--- branches/jG2fbook/src/de/g2fbook/main/Starter.java
+++ branches/jG2fbook/src/de/g2fbook/main/Starter.java
@@ -3,6 +3,7 @@
  */
 package de.g2fbook.main;
 
+import de.g2fbook.services.DatabaseService;
 import de.g2fbook.services.GoogleService;
 import de.g2fbook.services.I_PropertyService;
 import de.g2fbook.services.PropertyService;
@@ -19,16 +20,28 @@ public class Starter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 try {
+	
+	//Initialising
 	PropertyService.init("private.properties");
+	DatabaseService.init();
+	
+	//fetch some important parameter to connect to google
 	String usr=PropertyService.getProp(I_PropertyService.PARAM_GOOGLE_USR);
 	String pwd=PropertyService.getProp(I_PropertyService.PARAM_GOOGLE_PWD);
 	String url=PropertyService.getProp(I_PropertyService.PARAM_GOOGLE_URL_LOGIN);
 	
+	//connect to google and get the phonebook
 	GoogleService.logon(url,usr,pwd);
-	
 	url=PropertyService.getProp(I_PropertyService.PARAM_GOOGLE_URL_PBOOK);
 	url=url.replaceAll("PARAM_GOOGLE_USR", usr);
-	System.out.println(GoogleService.getPhoneBook(url));
+	String phoneBook=GoogleService.getPhoneBook(url);
+	
+	//put the phonebook to the database
+	DatabaseService.insertPhoneBook("googleBook", phoneBook);
+	
+	
+	//DatabaseService.query("SELECT * FROM phonebook;");
+	
 	
 } catch (Exception e) {
 	// TODO Auto-generated catch block
---
+++ branches/jG2fbook/src/de/g2fbook/services/DatabaseService.java
@@ -0,0 +1,63 @@
+package de.g2fbook.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class DatabaseService {
+	private static Connection dbConnection = null;
+	private static Statement statement = null;
+
+	public static void init() throws Exception {
+		/*
+		 * 
+		 * TODO: put all to envFile
+		 *
+		 */
+		Class.forName("org.hsqldb.jdbcDriver");
+		dbConnection = DriverManager.getConnection(
+				"jdbc:hsqldb:mem:DB_g2fbook", "sa", "");
+		statement = dbConnection.createStatement();
+		execute("CREATE TABLE phoneBook (phonebook_source VARCHAR(50) NOT NULL PRIMARY KEY, phonebook CLOB NOT NULL)");
+		
+
+	}
+
+	public static void finish() throws Exception {
+
+		statement.close();
+		dbConnection.close();
+	}
+
+	public static void execute(String sql) throws Exception {
+		statement.execute(sql);
+	}
+	public static void query(String sql) throws Exception {
+		
+		
+		  ResultSet rs=statement.executeQuery(sql);
+		  while(rs.next()) System.out.println(rs.getString(1)+"--"+rs.getString(2));
+		 
+	}
+	public static void main(String[] args) throws Exception {
+		try {
+			DatabaseService.init();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	public static void insertPhoneBook(String phonebook_source,String phonebook) throws Exception{
+		 PreparedStatement preparedStatement = dbConnection.prepareStatement("INSERT INTO phoneBook VALUES(?,?)");
+        preparedStatement.setString(1, phonebook_source);
+        preparedStatement.setString(2, phonebook);
+        preparedStatement.execute();
+       
+     
+        preparedStatement.close();
+        dbConnection.commit();
+	}
+}
